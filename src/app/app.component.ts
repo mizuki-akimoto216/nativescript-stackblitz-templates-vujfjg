7ec699a8459800f7057b8c1c62b56d1556f3e326
mizuki-akimoto216 <119698616+mizuki-akimoto216@users.noreply.github.com>
2023-06-29 16:32:32 -0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ns-app',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  myTitle = 'This title is from the component';
+  fruits = ['Banana', 'Apple', 'Orange', 'Strawberry', 'Mango'];
+}
