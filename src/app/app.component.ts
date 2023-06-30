4a0d8fa6bdb6ffc1d29a53810e30a5b0c3dccc11
mizuki-akimoto216 <119698616+mizuki-akimoto216@users.noreply.github.com>
2023-06-29 17:06:28 -0700

    Added an extra project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   myTitle = 'This title is from the component';
-  fruits = ['Banana', 'Apple', 'Orange', 'Strawberry', 'Mango'];
+  products: any[] = [
+    {
+      id: 1,
+      title: 'NNNNN',
+      price: 100,
+      image:
+        'https://images.unsplash.com/photo-1596405367208-63505402f113?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHRlbGV2aXNpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius lorem felis, a aliquam orci malesuada sit amet. Donec consequat nunc id sem euismod, sit amet mattis arcu elementum.',
+    },
+    {
+      id: 2,
+      title: 'MMMMM',
+      price: 200,
+      image:
+        'https://images.unsplash.com/photo-1595935736128-db1f0a261263?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHRlbGV2aXNpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius lorem felis, a aliquam orci malesuada sit amet. Donec consequat nunc id sem euismod, sit amet mattis arcu elementum.',
+    },
+    {
+      id: 3,
+      title: 'LLLLL',
+      price: 300,
+      image:
+        'https://images.unsplash.com/photo-1558888401-3cc1de77652d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHRlbGV2aXNpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius lorem felis, a aliquam orci malesuada sit amet. Donec consequat nunc id sem euismod, sit amet mattis arcu elementum.',
+    },
+    {
+      id: 3,
+      title: 'LLLLL',
+      price: 300,
+      image:
+        'https://images.unsplash.com/photo-1558888401-3cc1de77652d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHRlbGV2aXNpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius lorem felis, a aliquam orci malesuada sit amet. Donec consequat nunc id sem euismod, sit amet mattis arcu elementum.',
+    },
+  ];
 }
